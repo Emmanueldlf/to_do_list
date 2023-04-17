183884bdfd9c4fade1a41c0623e14d5047739ed2
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-04-17 18:16:18 +0900

    basic to do list in python

--- task.py
+++ task.py
@@ -1,24 +1,50 @@
+import json
+from json import JSONEncoder
+
 class Task():
 
     def __init__(self):
       self.tasks = []
+      self.task = {}
+      self.filename = 'tasks.json'
 
     def show_tasks(self):
+        with open(self.filename) as f_obj:
+            self.tasks_list = json.load(f_obj)
         for index, task in enumerate(self.tasks):
-            print(f"{index + 1}- {task.title()}")
+            print(f"{index + 1}- {task['name'].title()} {task['status']}")
+
+    def show_task(self):
+        with open(self.filename) as f_obj:
+            self.tasks_list = json.load(f_obj)
+            self.task_number = (int(input("What's the index of the task you want to check? ")))
+            print(f"{self.task_number}- {self.tasks[self.task_number - 1]}")
+
 
     def add_task(self):
+        self.task = {}
         if self.tasks == []:
-            self.task = input('enter a to do: ')
+            self.task['name'] = input('Enter your first task: ')
+            # self.task['description'] = input('Describe briefly your first task: ')
+            self.task['status'] = '[ ]'
             self.tasks.append(self.task)
         else:
-            self.task = input('Please enter a new task: ')
+            self.task['name'] = input('Please enter a new task: ')
+            self.task['status'] = ' [ ]'
             self.tasks.append(self.task)
         return self.tasks
 
+    def save_task(self):
+       with open(self.filename, 'w') as f_obj:
+            json.dump(self.tasks, f_obj)
+
     def edit_task(self):
         self.show_tasks()
-        self.task_number = (int(input("What's the index of the task you want to edit?"))) - 1
-        # task_number -= 1
+        self.task_number = (int(input("What's the index of the task you want to edit?")))
         self.new_task = input("Please enter the new task that will replace the one you picked: ")
-        self.tasks[self.task_number] = self.new_task
+        self.tasks[self.task_number - 1] = self.new_task
+
+    def mark_task(self):
+        self.show_tasks()
+        self.task_number = (int(input("What's the index of the task you want to mark as completed? ")))
+        self.tasks[self.task_number - 1]['status']=  " [X]"
--- to_do_list.py
+++ to_do_list.py
@@ -4,15 +4,21 @@ active = True
 task = Task()
 
 while active:
-    user_action = input("Will you add a task? edit a task? See your list?")
+    user_action = input("Will you add a task? edit a task? Mark a task as completed? See your list? Check a specific task? ")
     match user_action:
         case 'Add':
-            # task = Task()
             task.add_task()
+            task.save_task()
         case 'Edit':
             task.edit_task()
+            task.save_task()
+        case 'Mark':
+            task.mark_task()
+            task.save_task()
         case 'See':
             task.show_tasks()
+        case 'Check':
+            task.show_task()
         case 'Quit':
             active = False
 
