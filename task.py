799d2a9fbf5a83bfbb8ae2c351ce9205625f4521
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-04-18 17:22:08 +0900

    create a task, save it in a json, show tasks or a specific task from the json and edit or mark a task stored in the json

--- task.py
+++ task.py
@@ -10,15 +10,16 @@ class Task():
 
     def show_tasks(self):
         with open(self.filename) as f_obj:
-            self.tasks_list = json.load(f_obj)
+            self.tasks = json.load(f_obj)
         for index, task in enumerate(self.tasks):
             print(f"{index + 1}- {task['name'].title()} {task['status']}")
+        return self.tasks
 
     def show_task(self):
         with open(self.filename) as f_obj:
             self.tasks_list = json.load(f_obj)
             self.task_number = (int(input("What's the index of the task you want to check? ")))
-            print(f"{self.task_number}- {self.tasks[self.task_number - 1]}")
+            print(f"{self.task_number}- {self.tasks_list[self.task_number - 1]['name'].title()} {self.tasks_list[self.task_number - 1]['status']}")
 
 
     def add_task(self):
@@ -35,14 +36,14 @@ class Task():
         return self.tasks
 
     def save_task(self):
-       with open(self.filename, 'w') as f_obj:
+        with open(self.filename, 'w') as f_obj:
             json.dump(self.tasks, f_obj)
 
     def edit_task(self):
         self.show_tasks()
-        self.task_number = (int(input("What's the index of the task you want to edit?")))
+        self.task_number = (int(input("What's the index of the task you want to edit? ")))
         self.new_task = input("Please enter the new task that will replace the one you picked: ")
-        self.tasks[self.task_number - 1] = self.new_task
+        self.tasks[self.task_number - 1]['name'] = self.new_task
 
     def mark_task(self):
         self.show_tasks()
