1ed78f12fa041139b436d9460ccb1a64e1ba81a1
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-04-27 09:52:57 +0900

    added a GUI and solved previous issues

--- app/to_do_gui.py
+++ app/to_do_gui.py
@@ -1,9 +1,14 @@
-from task import *
-from PySimpleGUI import *
+from task import Task
+import PySimpleGUI as psg
+# import matplotlib
+# matplotlib.use('Agg')
 
-label = PySimpleGUI.Text("Enter a task you would like to add to your To Do List")
+label = psg.Text("Enter a task you would like to add to your To Do List")
+input_box = psg.InputText(tooltip="Enter a task")
+add_button = psg.Button("Add")
 
 
-window = PySimpleGUI.Window('To Do List', layout=[""])
+window = psg.Window('To Do List', layout=[[label], [input_box, add_button]])
 window.read()
+print("Welcome to our Tasks app")
 window.close()
